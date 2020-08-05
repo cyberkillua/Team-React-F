c1c29b042383da01f3b08352f24f48ac97a9b703
Lateef Quadri <Lateef9816@gmail.com>
2020-08-05 19:06:34 +0100

    Installed react-router-dom and wrote paths for Homepage, Albums, Artists, Profile and Signin SignUp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Components
 import Homepage from "./pages/Homepage/Homepage";
 import Album from "./pages/Album/Album";
+import Artists from "./pages/Artists/Artists";
+import Profile from "./pages/Profile/Profile";
+import SigninSignup from "./pages/Signin-Signup/SigninSignup";
 import "./App.css";
 
 function App() {
-  return <Homepage />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/albums" component={Album} />
+        <Route path="/artists" component={Artists} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/signin" component={SigninSignup} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
