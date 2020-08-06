534adbfa5605deee746e7c2ddd0d6ad8d6137953
Lateef Quadri <Lateef9816@gmail.com>
2020-08-06 22:37:26 +0100

    Minor fix on Homepage.jsx

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 // Componets
 import Sidebar from "../../components/Sidebar/Sidebar";
@@ -7,30 +7,24 @@ import Featured from "../../components/Featured/Featured";
 // CSS
 import "./Homepage.css";
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-2" id="sidebar">
-            <Sidebar />
+const HomePage = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-2" id="sidebar">
+          <Sidebar />
+        </div>
+        <div className="col-10 px-0 w-100">
+          <div className="w-100">
+            <Featured />
           </div>
-          <div className="col-10 px-0 w-100">
-            <div className="w-100">
-              <Featured />
-            </div>
-            <div className="w-100">
-              {/* <Tracks tracks={this.props.tracks} /> */}
-            </div>
+          <div className="w-100">
+            {/* <Tracks tracks={this.props.tracks} /> */}
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default HomePage;
