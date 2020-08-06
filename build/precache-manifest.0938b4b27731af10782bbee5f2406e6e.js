5a3b3298ae9a1e7ac69402dffbb58c6e52908298
Lateef Quadri <Lateef9816@gmail.com>
2020-08-06 10:10:58 +0100

    First build files

---
+++ build/precache-manifest.0938b4b27731af10782bbee5f2406e6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a111440c070d944e8f7a0d35f97e02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "992dff40649f3be7b457",
+    "url": "/static/css/main.31cebc6d.chunk.css"
+  },
+  {
+    "revision": "a22af189f2e5a790b936",
+    "url": "/static/js/2.7fc57689.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7fc57689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992dff40649f3be7b457",
+    "url": "/static/js/main.0f595e84.chunk.js"
+  },
+  {
+    "revision": "22d67fdb6a1596836ede",
+    "url": "/static/js/runtime-main.994d8cee.js"
+  },
+  {
+    "revision": "14e2b11c8d4734ef671433e7f6efe9dc",
+    "url": "/static/media/bg-3.14e2b11c.png"
+  },
+  {
+    "revision": "c181d30a50a112b52ce5b9e093ae9d45",
+    "url": "/static/media/headset.c181d30a.png"
+  }
+]);
